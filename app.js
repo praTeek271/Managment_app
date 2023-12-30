7d5866426343fe3d4d0f9e879ccc09d4e21594e6
prateek271 <prateekpatriotasme@gmail.com>
2023-12-30 19:33:06 +0530

    first run

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ const expressLayout = require("express-ejs-layouts");
 
 const app = express();
 const port = 5000 || process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static files
+app.use(express.static("public"));
+
+//teemplating engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+//home route
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
